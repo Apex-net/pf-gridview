a03245e3138e0ccd4b42596e5ebbf228683d54de
Alessandro Zoffoli <a.zoffoli@apexnet.it>
2014-09-30 18:39:25 +0200

    Add missing macros to compare current iOS version

--- PFGridView/PFGridView/PFGridViewSection+Internal.m
+++ PFGridView/PFGridView/PFGridViewSection+Internal.m
@@ -10,6 +10,12 @@
 #import "PFGridIndexPath.h"
 #import "PFGridView+Internal.h"
 
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
 @implementation PFGridViewSection (PFGridViewSection_Internal)
 
 #pragma mark - Used by PFGridView
